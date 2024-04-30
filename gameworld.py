ef75099c4c3729074c112055df9af5ba4dc2abd1
SaadNazir1 <96564519+SaadNazir1@users.noreply.github.com>
2024-04-30 19:54:39 +0200

    Adding list of Food, Weapon, Boat and Treasure

--- gameworld.py
+++ gameworld.py
@@ -5,6 +5,10 @@ class GameWorld:
         self.players = []
         self.npcs = []
         self.defeaters = []
+        self.food = []
+        self.weapons = []
+        self.treasures = []
+        self.boat = []
 
     def add_location(self, location):
         self.locations[location.name] = location
@@ -17,6 +21,18 @@ class GameWorld:
 
     def add_defeater(self, defeater):
         self.defeaters.append(defeater)
+
+    def add_food(self, Food):
+        self.food.append(Food)
+
+    def add_weapon(self, Weapon):
+        self.weapons.append(Weapon)
+
+    def add_treasures(self, Treasure):
+        self.treasures.append(Treasure)
+
+    def add_boats(self, Boat):
+        self.boat.append(Boat)
 
     def get_location_description(self, position):
         for location in self.locations.values():
