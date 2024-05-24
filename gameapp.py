bef1a2551606f619c3d1241c88edfdf6da4a757d
SaadNazir1 <96564519+SaadNazir1@users.noreply.github.com>
2024-05-24 20:15:39 +0200

    Update gameapp.py

--- gameapp.py
+++ gameapp.py
@@ -1,83 +1,25 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog
-from defeater import Defeater
 from gameworld import GameWorld
-from location import Location
+from location import StartingPoint, DarkCave, DinoNest, GreenForest,RipleRiver
 from player import Player
-from Item import Food, Weapon, Boat,Treasure
 
 # Main Game Logic
 class GameApp(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.location_list = []
 
         self.title("Mystical Forest Adventure")
 
         self.game_world = GameWorld()
 
         # Initialize locations
-        initial_location = Location("Starting Point", "Beginning of the adventure.")
-        initial_location.set_position((0, 0))
-        green_forest = Location("Green Forest", "Lush vegetation and vibrant flora.")
-        green_forest.set_position((1, 0))
-        dark_cave = Location("Dark Cave", "A mysterious and shadowy underworld.")
-        dark_cave.set_position((-1, 0))
-        dino_nest = Location("Dino Nest", "A prehistoric sanctuary.")
-        dino_nest.set_position((0, 1))
-        ripple_river = Location("Ripple River", "A serene and crystal-clear stream.")
-        ripple_river.set_position((0, -1))
-
-        self.game_world.add_location(initial_location)
-        self.game_world.add_location(green_forest)
-        self.game_world.add_location(dark_cave)
-        self.game_world.add_location(dino_nest)
-        self.game_world.add_location(ripple_river)
-
-        # Initialize weapons
-        dinosaur = "Dinosaur"
-        magical_potion = "Magical Potion"
-        sword = Weapon("Sword","A katana samurai sword with dragon handle",50)
-        gun = Weapon("Shotgun","Purdey Sidelock Shotgun",50)
-        stick = Weapon("Stick","A thin wooden stick",10)
-
-        # Initialize defeaters
-        dragon = Defeater("Dragon", 50, dinosaur, (1, 1))
-        monster = Defeater("Monster", 30, None, (-1, -1))
-        fairy = Defeater("Fairy", 10, None, (2, 2))
-        enchanted_guardian = Defeater("Enchanted Guardian", 40, None, (-2, -2))
-
-        #Initialize foods
-        apple = Food("Apple","A big red apple",20)
-        pizza = Food ("Pizza", "A loaded cheese pizza with chicken cubes",100)
-
-        #Initialize treasure
-        diamonds = Treasure("Diamonds","A pouch of diamonds",1000)
-        coins = Treasure("Coins","A bag of silver coins",200)
-
-        #Initialize the boat
-        boat = Boat("A white speedboat")
-
-        #Initialize
-        self.game_world.add_defeater(dragon)
-        self.game_world.add_defeater(monster)
-        self.game_world.add_defeater(fairy)
-        self.game_world.add_defeater(enchanted_guardian)
-
-        #Add the food
-        self.game_world.add_food(apple)
-        self.game_world.add_food(pizza)
-
-        #Add the weapons
-        self.game_world.add_weapon(stick)
-        self.game_world.add_weapon(gun)
-        self.game_world.add_weapon(sword)
-
-        #Add the boat
-        self.game_world.add_boats(boat)
-
-        #Add the treasures
-        self.game_world.add_treasures(coins)
-        self.game_world.add_treasures(diamonds)
+        self.location_list.append(StartingPoint)
+        self.location_list.append(GreenForest)
+        self.location_list.append(DinoNest)
+        self.location_list.append(DarkCave)
+        self.location_list.append(RipleRiver)
 
         # Initialize player
         player_name = simpledialog.askstring("Input", "Enter your name:")
@@ -86,12 +28,6 @@ class GameApp(tk.Tk):
         self.player = Player(player_name, 100, 10, initial_position)
         self.game_world.add_player(self.player)
 
-        # # Initialize player
-        # player_name = simpledialog.askstring("Input", "Enter your name:")
-        # # Set the initial position of the player to match an existing location
-        # initial_position = (0, 0)  # Adjust the initial position to match an existing location
-        # self.player = Player(player_name, 100, 10, initial_position)
-        # self.game_world.add_player(self.player)
 
         # Create GUI elements
         self.label = tk.Label(self, text=self.game_world.get_location_description(self.player.position))
