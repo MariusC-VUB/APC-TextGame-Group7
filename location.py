4c213dc9e4f58260ca201c65f9e2382e875efc1d
SaadNazir1 <96564519+SaadNazir1@users.noreply.github.com>
2024-05-24 02:01:11 +0200

    Added more methods

--- location.py
+++ location.py
@@ -1,13 +1,44 @@
 # Location Class
 class Location:
-    def __init__(self, name, description):
-        self.name = name
-        self.position = None
-        self.description = description
+    def __init__(self, x_coordinate, y_coordinate):
         self.defeater = None
-
-    def set_position(self, position):
-        self.position = position
+        self.food = []
+        self.weapons = []
+        self.boats = []
+        self.treasures = []
+        self.correct_defensive_action = None
 
     def set_defeater(self, defeater):
         self.defeater = defeater
+
+    def add_food(self, food):
+        self.food.append(food)
+
+    def add_weapons(self, weapons):
+        self.weapons.append(weapons)
+
+    def add_treasures(self, treasure):
+        self.treasures.append(treasure)
+
+    def add_boat(self, boat):
+        self.boats.append(boat)
+
+    def get_boats(self):
+        return self.boats
+
+    def get_food(self):
+        return self.food
+
+    def get_weapons(self):
+        return self.weapons
+
+    def get_treasures(self):
+        return self.treasures
+
+    def get_correct_defensive_action(self):
+        return self.correct_defensive_action
+
+    # Set the correct defensive action against the currently attacking enemy.
+    def set_correct_defensive_action(self, action):
+        self.correct_defensive_action = action
+
