f9faeb1d6643a05a9bb775190d95996f212e92c3
SaadNazir1 <96564519+SaadNazir1@users.noreply.github.com>
2024-05-25 17:51:40 +0200

    Update player.py

--- player.py
+++ player.py
@@ -1,6 +1,5 @@
-import random
 from tkinter import messagebox
-from Item import Food, Weapon, Treasure, Boat
+import random
 
 class Player:
     def __init__(self, name, hp, agility, position, game_world):
@@ -32,24 +31,6 @@ class Player:
         else:
             messagebox.showinfo("Move", "Invalid direction!")
 
-    def use_item(self, item_name):
-        item = next((item for item in self.inventory if item.name == item_name), None)
-        if item:
-            self.inventory.remove(item)
-            if isinstance(item, Food):
-                self.hp += item.strength
-                messagebox.showinfo("Use Item", f"You used {item_name} and gained {item.strength} HP.")
-            elif isinstance(item, Weapon):
-                self.extra_strength += item.strength
-                messagebox.showinfo("Use Item", f"You used {item_name} and gained {item.strength} extra strength.")
-            elif isinstance(item, Treasure):
-                self.score += item.value
-                messagebox.showinfo("Use Item", f"You used {item_name} and gained {item.value} points.")
-            elif isinstance(item, Boat):
-                messagebox.showinfo("Use Item", f"You used {item_name} to travel on water.")
-        else:
-            messagebox.showinfo("Use Item", f"{item_name} is not in your inventory.")
-
     def take_food(self, food_name):
         current_location = self.game_world.locations.get(self.position)
         if current_location:
@@ -66,7 +47,7 @@ class Player:
         if food:
             self.inventory.remove(food)
             self.hp += food.strength
-            messagebox.showinfo("Eat Food", f"You ate {food_name} and gained {food.strength} HP.")
+            messagebox.showinfo("Eat Food", f"You ate {food_name} and gained {food.strength} health.")
         else:
             messagebox.showinfo("Eat Food", f"{food_name} is not in your inventory.")
 
@@ -85,6 +66,7 @@ class Player:
         weapon = next((weapon for weapon in self.inventory if weapon.name == weapon_name), None)
         if weapon:
             self.extra_strength += weapon.strength
+            self.inventory.remove(weapon)
             messagebox.showinfo("Use Weapon", f"You used {weapon_name} and gained {weapon.strength} extra strength.")
         else:
             messagebox.showinfo("Use Weapon", f"{weapon_name} is not in your inventory.")
@@ -100,10 +82,22 @@ class Player:
             else:
                 messagebox.showinfo("Take Treasure", f"{treasure_name} is not available here.")
 
+    def use_treasure(self, treasure_name):
+        treasure = next((treasure for treasure in self.inventory if treasure.name == treasure_name), None)
+        if treasure:
+            self.score += treasure.value
+            self.inventory.remove(treasure)
+            messagebox.showinfo("Use Weapon", f"You used {treasure_name} and gained {treasure.value} scores.")
+        else:
+            messagebox.showinfo("Use Weapon", f"{treasure_name} is not in your inventory.")
+
     def attack(self, enemy):
         player_damage = self.agility + self.extra_strength + random.randint(0, 10)
         enemy.hp -= player_damage
         if enemy.hp <= 0:
             messagebox.showinfo("Attack", f"You defeated the {enemy.name}!")
         else:
-            messagebox.showinfo("Attack", f"You attacked the {enemy.name} for {player_damage} damage. {enemy.hp} HP remaining.")
+            messagebox.showinfo("Attack",
+                                f"You attacked the {enemy.name} for {player_damage} damage. {enemy.hp} HP remaining.")
+
+
