48b5c05d6616b3ff8592d720cbad5ddb3b9894b8
SaadNazir1 <96564519+SaadNazir1@users.noreply.github.com>
2024-05-25 17:51:08 +0200

    Update defeater.py

--- defeater.py
+++ defeater.py
@@ -5,8 +5,12 @@ class Defeater:
         self.strength = strength
 
     def attack(self, player):
-        player.hp -= self.strength
-        if player.hp <= 0:
-            player.hp = 0
-            return f"The {self.name} hits you for {self.strength} damage. You have been defeated."
-        return f"The {self.name} hits you for {self.strength} damage. You have {player.hp} HP left."
+        if player.extra_strength > 0:
+            player.extra_strength -= self.strength
+            return f"The {self.name} hits you for {self.strength} damage. You have {player.extra_strength} extra strength left."
+        else:
+            self.hp -= self.strength
+            if self.hp <= 0:
+                self.hp = 0
+                return f"The {self.name} hits you for {self.strength} damage. You have been defeated."
+            return f"The {self.name} hits you for {self.strength} damage. You have {self.hp} HP left."
