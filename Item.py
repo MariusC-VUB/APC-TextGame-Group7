fac7619d03accab859aeaec1c3fdd1dabe1c4846
SaadNazir1 <96564519+SaadNazir1@users.noreply.github.com>
2024-05-25 17:57:46 +0200

    remove boat

--- Item.py
+++ Item.py
@@ -4,20 +4,19 @@ class Item:
         self.description = description
         self.strength = strength
 
+
 class Food(Item):
     def __init__(self, name, description, strength):
         super().__init__(name, description, strength)
 
+
 class Weapon(Item):
     def __init__(self, name, description, strength):
         super().__init__(name, description, strength)
+
 
 class Treasure:
     def __init__(self, name, description, value):
         self.name = name
         self.description = description
         self.value = value
-
-class Boat:
-    def __init__(self, name):
-        self.name = name
