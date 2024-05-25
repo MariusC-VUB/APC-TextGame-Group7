7159ed9aac28d2299f033fcf0d707931634c05fe
SaadNazir1 <96564519+SaadNazir1@users.noreply.github.com>
2024-05-25 17:50:27 +0200

    Added the new buttons. Exit, Scores.
    Added the new buttons. Exit, Scores. GUI will be terminated if player lose

--- gameapp.py
+++ gameapp.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog
 from gameworld import GameWorld
-from location import StartingPoint, DarkCave, DinoNest, GreenForest, RipleRiver
+from location import StartingPoint, DarkCave, DinoNest, GreenForest, RippleRiver
 from player import Player
 
 class GameApp(tk.Tk):
@@ -15,7 +15,7 @@ class GameApp(tk.Tk):
         self.game_world.add_location(GreenForest())
         self.game_world.add_location(DinoNest())
         self.game_world.add_location(DarkCave())
-        self.game_world.add_location(RipleRiver())
+        self.game_world.add_location(RippleRiver())
 
         # Initialize player
         player_name = simpledialog.askstring("Input", "Enter your name:")
@@ -30,7 +30,7 @@ class GameApp(tk.Tk):
         self.move_button = tk.Button(self, text="Move", command=self.move_dialog)
         self.move_button.pack(pady=20)
 
-        self.use_item_button = tk.Button(self, text="Use Item", command=self.use_item_dialog)
+        self.use_item_button = tk.Button(self, text="Use Treasure", command=self.use_treasure_dialog)
         self.use_item_button.pack(pady=20)
 
         self.attack_button = tk.Button(self, text="Attack", command=self.attack_dialog)
@@ -51,43 +51,62 @@ class GameApp(tk.Tk):
         self.pick_treasure_button = tk.Button(self, text="Pick Treasure", command=self.take_treasure_dialog)
         self.pick_treasure_button.pack(pady=20)
 
+        self.exit_button = tk.Button(self, text="Health and Score", command=self.check_attributes)
+        self.exit_button.pack(pady=20)
+
+        self.exit_button = tk.Button(self, text= "Exit", command= self.exit_dialog)
+        self.exit_button.pack(pady=20)
+
     def move_dialog(self):
         direction = simpledialog.askstring("Input", "Which direction? (north/south/east/west)")
-        self.player.move(direction)
+        self.player.move(direction.lower())
         self.label.config(text=self.game_world.get_location_description(self.player.position))
 
-    def use_item_dialog(self):
-        item = simpledialog.askstring("Input", "Which item?")
-        self.player.use_item(item)
+    def use_treasure_dialog(self):
+        treasure = simpledialog.askstring("Input", "Which item?")
+        self.player.use_treasure(treasure.lower())
 
     def take_food_dialog(self):
         food = simpledialog.askstring("Input", "Which food?")
-        self.player.take_food(food)
+        self.player.take_food(food.lower())
 
     def eat_food_dialog(self):
         eat_food = simpledialog.askstring("Input", "Which food?")
-        self.player.eat_food(eat_food)
+        self.player.eat_food(eat_food.lower())
 
     def take_weapon_dialog(self):
         weapon = simpledialog.askstring("Input", "Which weapon?")
-        self.player.take_weapon(weapon)
+        self.player.take_weapon(weapon.lower())
 
     def use_weapon_dialog(self):
         weapon = simpledialog.askstring("Input", "Which weapon?")
-        self.player.use_weapon(weapon)
+        self.player.use_weapon(weapon.lower())
 
     def take_treasure_dialog(self):
         treasure = simpledialog.askstring("Input", "Which treasure?")
-        self.player.take_treasure(treasure)
+        self.player.take_treasure(treasure.lower())
 
     def attack_dialog(self):
-        current_location = self.game_world.locations.get(self.player.position)
-        if current_location and current_location.defeater:
-            enemy = current_location.defeater
-            self.player.attack(enemy)
-            if enemy.hp > 0:
-                result = enemy.attack(self.player)
-                messagebox.showinfo("Attack Result", result)
-            self.label.config(text=self.game_world.get_location_description(self.player.position))
+        if self.player.hp > 0:
+            current_location = self.game_world.locations.get(self.player.position)
+            if current_location and current_location.defeater:
+                enemy = current_location.defeater
+                self.player.attack(enemy)
+                if enemy.hp > 0:
+                    result = enemy.attack(self.player)
+                    messagebox.showinfo("Attack Result", result)
+                self.label.config(text=self.game_world.get_location_description(self.player.position))
+            else:
+                messagebox.showinfo("Attack", "There's nothing to attack here.")
+
         else:
-            messagebox.showinfo("Attack", "There's nothing to attack here.")
+            messagebox.showinfo("Game Over", f"You scored {self.player.score}")
+            messagebox.showerror("Game Over")
+            self.quit()
+
+    def exit_dialog(self):
+        self.quit()
+
+    def check_attributes(self):
+        messagebox.showinfo("Health and Score",f"Your score is {self.player.score}, health is {self.player.hp}"
+                                               f" and strength is {self.player.extra_strength}")
